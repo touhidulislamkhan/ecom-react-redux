5f7c956d172f78c3a7c59c2b2af90f387522c933
touhidulislamkhan <touhidikhan0@gmail.com>
2021-04-15 23:36:45 +0600

    Created Reducers

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+    id: 1,
+    product: 'Hello',
+    description: ""
+}
+
+export const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_PRODUCTS':
+            return {
+                ...state,
+                products: action.payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+export const selectedProductsReducer = (state = {}, action) => {
+    // console.log(type);
+    switch (action.type) {
+        case 'SELECTED_PRODUCT':
+            return { ...state, ...action.payload };
+        //   case ActionTypes.REMOVE_SELECTED_PRODUCT:
+        // return {};
+        default:
+            return state;
+    }
+};
